d6913a5880b7bf04dae81206f62203fcd29c5ff8
vinh pham <vinh.pham@niteco.se>
2018-08-22 18:34:13 +0700

    Finish D2

--- AlloyAdvanced/Models/Pages/ContactPage.cs
+++ AlloyAdvanced/Models/Pages/ContactPage.cs
@@ -1,7 +1,11 @@
 using System.ComponentModel.DataAnnotations;
+using AlloyAdvanced.Business;
+using AlloyAdvanced.Business.EditorDescriptors;
 using AlloyAdvanced.Business.Rendering;
 using EPiServer.Web;
 using EPiServer.Core;
+using AlloyAdvanced.Business.Selectors;
+using EPiServer.Shell.ObjectEditing;
 
 namespace AlloyAdvanced.Models.Pages
 {
@@ -9,7 +13,6 @@ namespace AlloyAdvanced.Models.Pages
     /// Represents contact details for a contact person
     /// </summary>
     [SiteContentType(
-        GUID = "F8D47655-7B50-4319-8646-3369BA9AF05B",
         GroupName = Global.GroupNames.Specialized)]
     [SiteImageUrl(Global.StaticGraphicsFolderPath + "page-type-thumbnail-contact.png")]
     public class ContactPage : SitePageData, IContainerPage
@@ -24,5 +27,34 @@ namespace AlloyAdvanced.Models.Pages
         [Display(GroupName = Global.GroupNames.Contact)]
         [EmailAddress]
         public virtual string Email { get; set; }
+
+        [Display(
+            Name = "Region",
+            GroupName = Global.GroupNames.Contact,
+            Order = 10)]
+        [SelectManyEnum(typeof(Region))]
+        public virtual Region Region { get; set; }
+
+        [Display(
+            Name = "YouTube video",
+            GroupName = Global.GroupNames.Contact,
+            Order = 20)]
+        [SelectOne(SelectionFactoryType = typeof(YouTubeSelectionFactory))]
+        public virtual string YouTubeVideo { get; set; }
+
+        [Display(
+            Name = "Home city",
+            GroupName = Global.GroupNames.Contact,
+            Order = 30)]
+        //[SelectOne(SelectionFactoryType = typeof(CitySelectionFactory))]
+        [UIHint(Global.SiteUIHints.City)]
+        public virtual string HomeCity { get; set; }
+
+        [Display(
+            Name = "Other cities",
+            GroupName = Global.GroupNames.Contact,
+            Order = 40)]
+        [SelectMany(SelectionFactoryType = typeof(CitySelectionFactory))]
+        public virtual string OtherCities { get; set; }
     }
 }
---
+++ AlloyAdvanced/Models/Region.cs
@@ -0,0 +1,12 @@
+﻿namespace AlloyAdvanced.Models
+{
+    [System.Flags]
+    public enum Region
+    {
+        Unknown,    // 0
+        Europe,     // 1
+        UK,         // 2
+        USA,        // 3
+        RestOfWorld // 4
+    }
+}
